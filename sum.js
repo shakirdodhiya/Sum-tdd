66f41202f981208919fb3ec9c3976ef204a86ca6
Shakir <Shakirdodhiya1120@gmail.com>
2025-03-11 14:35:37 +0530

    Add sum method

--- sum.js
+++ sum.js
@@ -1,5 +1,13 @@
 exports.add = (numbers) => {
   if(!Boolean(numbers.trim())){
     return 0;
+  }else{
+    let sum = 0;
+    let numbers_arr = numbers.split(',');
+    numbers_arr.forEach((num_value) => {
+      sum += parseInt(num_value.trim(), 10)
+    })
+
+    return sum
   }
 }
--- sum.test.js
+++ sum.test.js
@@ -5,3 +5,9 @@ describe('Add function with blank("") value passed', () => {
     expect(add("")).toBe(0);
   })
 })
+
+describe('Add function with "15,6" value passed', () => {
+  it('Should return 21', () => {
+    expect(add("15,6")).toBe(21);
+  })
+})
