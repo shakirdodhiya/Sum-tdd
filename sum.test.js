1f8a5fb93d430957c9a6dae1c4acaddf170583b9
Shakir <Shakirdodhiya1120@gmail.com>
2025-03-11 15:08:20 +0530

    Renamed test cases

--- sum.test.js
+++ sum.test.js
@@ -1,51 +1,51 @@
 const { add } = require('./sum');
 
-describe('Add function with blank("") value passed', () => {
+describe('Check blank("") value', () => {
   it('Should return 0', () => {
     expect(add("")).toBe(0);
   })
 })
 
-describe('Add function with "15,6" value passed', () => {
+describe('2 numbers passed', () => {
   it('Should return 21', () => {
     expect(add("15,6")).toBe(21);
   })
 })
 
-describe('Add function with "15\\n6" value passed', () => {
+describe('Numbers with "\\n"', () => {
   it('Should return 21', () => {
     expect(add("15\n6")).toBe(21);
   })
 })
 
 // Delimiter '-'
-describe('Add function with "//-10\\n6-5" value passed', () => {
+describe('Numbers with custom delimiter "-"', () => {
   it('Should return 21', () => {
     expect(add("//-10\n6-5")).toBe(21);
   })
 })
 
 // Delimiter '.'
-describe('Add function with "//.10\\n6.5" value passed', () => {
+describe('Numbers with custom delimiter "."', () => {
   it('Should return 21', () => {
     expect(add("//.10\n6.5")).toBe(21);
   })
 })
 
 // Delimiter '@'
-describe('Add function with "//@10\\n6@5" value passed', () => {
+describe('Numbers with custom delimiter "@"', () => {
   it('Should return 21', () => {
     expect(add("//@10\n6@5")).toBe(21);
   })
 })
 
-describe('Add function with "15,-6,1,-5,2" value passed', () => {
+describe('Check negative numbers', () => {
   it('Should return "negative numbers not allowed -6,-5"', () => {
     expect(add("15,-6,1,-5,2")).toBe("negative numbers not allowed -6,-5");
   })
 })
 
-describe('Add function with "15,6,1221" value passed', () => {
+describe('Ignore value greater than 1000', () => {
   it('Should return 21', () => {
     expect(add("15,6,1221")).toBe(21);
   })
